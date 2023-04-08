d7441b90a64e67d670b015cca96b32c16179abe7
Alex-YuYan <alex.yan@nyu.edu>
2023-04-08 15:45:15 -0400

    feat: register working

--- Frontend/src/Pages/CustomerRegisterButton.jsx
+++ Frontend/src/Pages/CustomerRegisterButton.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Dialog, Transition } from '@headlessui/react';
 import { RiErrorWarningLine } from 'react-icons/ri';
+import axios from '../axios';
+import md5 from 'blueimp-md5';
 
 const CustomerRegisterButton = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,10 +11,24 @@ const CustomerRegisterButton = () => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    // md5 hash the password
+    data.password = md5(data.password);
+    try {
+      const response = await axios.post('/customer/register', data, { withCredentials: true })
+      if (response.data.success === true) {
+        alert('Registration successful, please log in.');
+        setIsOpen(false);
+        reset();
+      } else {
+        alert(response.data.error);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -66,15 +82,35 @@ const CustomerRegisterButton = () => {
                   <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="mt-4">
                       <label
-                        htmlFor="username"
+                        htmlFor="first_name"
                         className="block text-sm font-medium text-gray-700"
                       >
-                        Username
+                        First Name
                       </label>
                       <input
-                        {...register('username', { required: true })}
+                        {...register('first_name', { required: true })}
                         type="text"
-                        id="username"
+                        id="first_name"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                      />
+                      {errors.username && (
+                        <div className="flex items-center mt-2 text-sm text-red-600">
+                          <RiErrorWarningLine className="mr-1" />
+                          <span>Username is required</span>
+                        </div>
+                      )}
+                    </div>
+                    <div className="mt-4">
+                      <label
+                        htmlFor="last_name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Last Name
+                      </label>
+                      <input
+                        {...register('last_name', { required: true })}
+                        type="text"
+                        id="last_name"
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
                       />
                       {errors.username && (
@@ -137,7 +173,7 @@ const CustomerRegisterButton = () => {
                     </div>
                     <div className="mt-6">
                       <button
-                        type="button"
+                        type="submit"
                         className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                       >
                         Register
