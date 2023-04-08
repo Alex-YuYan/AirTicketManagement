caf761e8e26fc6374e73f5232c995b51e5071be5
Alex-YuYan <alex.yan@nyu.edu>
2023-04-07 23:47:25 -0400

    build: set up basic database tool class and example endpoint

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
+from db import Database
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+app.config.from_mapping(
+    DB_NAME="flightmgr",
+    DB_USER="root",
+    DB_PASSWORD="root",
+    DB_HOST="127.0.0.1",
+    DB_PORT="8889",
+)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+db = Database()
+db.init_app(app)
 
-if __name__ == '__main__':
+cors = cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/example")
+def example():
+    query = "SELECT * FROM Flight WHERE flight_number = :flight_number"
+    result = db.execute(query, {"flight_number": 211}, fetch=True)
+    return jsonify(result)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ Backend/db.py
@@ -0,0 +1,36 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+
+class Database:
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.config["SQLALCHEMY_DATABASE_URI"] = self.create_db_uri(
+            app.config["DB_USER"],
+            app.config["DB_PASSWORD"],
+            app.config["DB_HOST"],
+            app.config["DB_PORT"],
+            app.config["DB_NAME"],
+        )
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        self.db = SQLAlchemy(app)
+        app.teardown_appcontext(self.teardown)
+
+    @staticmethod
+    def create_db_uri(user, password, host, port, name):
+        return f"mysql+pymysql://{user}:{password}@{host}:{port}/{name}"
+
+    def execute(self, query, params=None, fetch=False):
+        with self.db.engine.connect() as connection:
+            result_proxy = connection.execute(text(query), params)
+            if fetch:
+                rows = result_proxy.fetchall()
+                column_names = result_proxy.keys()
+                return [dict(zip(column_names, row)) for row in rows]
+        return []
+
+    def teardown(self, exception):
+        self.db.session.remove()
